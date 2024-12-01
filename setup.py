2e17ed4d1c73ef3a7b8584045824be03675fe25f
George Kouretas <thegreekgeorge07@gmail.com>
2024-11-30 18:14:25 -0800

    Added entry point

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'user_input_node = pi-user-input-node.user_input_node:main'
         ],
     },
 )
