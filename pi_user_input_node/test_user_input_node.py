89a4ef14c1e2f556dd211f696658e86285743366
George Kouretas <thegreekgeorge07@gmail.com>
2024-12-08 12:33:50 -0800

    Removed previous declarations

--- pi_user_input_node/test_user_input_node.py
+++ pi_user_input_node/test_user_input_node.py
@@ -17,31 +17,9 @@ from user_input_node_config import *
 def test_button_callback(button_description: str):
     print(f"button {button_description} pressed")
     
-def test_encoder_callback():
-    print(f"encoder value: {test_encoder.steps}")
-    
-test_button = Button(
-    pin = 4,
-    pull_up = True,
-    bounce_time = 0.1 # 100 ms debounce
-)
-
-test_encoder = RotaryEncoder(
-    a = 5,
-    b = 6,
-    bounce_time = 0.1, # 100 ms debounce
-    max_steps = 20
-)
-
-test_rgba_led = RGBLED(
-    red = 9,
-    green = 10,
-    blue = 11,
-    active_high = True,
-    pwm = True
-)
-
-test_rgba_led.color = (1, 1, 0)
+def test_encoder_callback(encoder: RotaryEncoder):
+    print(f"encoder value: {encoder.steps}")
+ 
 
 def main():
     button_objs: list[Button] = []
@@ -63,7 +41,7 @@ def main():
         bounce_time = 0.1 # 100 ms
     )
     
-    encoder_obj.when_activated = test_encoder_callback
+    encoder_obj.when_activated = partial(test_encoder_callback, encoder_obj)
     
     seven_segment_obj = LEDMultiCharDisplay(
         LEDCharDisplay(*SEVEN_SEGMENT_LED_PINS, active_high = True),
